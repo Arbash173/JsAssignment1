721fd6a1c4853f9db90390fbc776f182c6ff331b
Arbash173 <arbashhumayun0@gmail.com>
2022-05-11 07:06:15 +0500

    first commit

---
+++ chap1.js
@@ -0,0 +1,25 @@
+// chapter01 assignment
+alert ("welcome user")
+
+alert ("Error!Please enter a valid password.")
+alert ("Welcome to JS land...\nHappy Coding")
+
+alert("Welcome to JS land...")
+window.alert("Happy Coding")
+// chapter2 assignment
+var username;
+var Mynanme ="Mirza Arbash Baig";
+var message ="Hello World";
+alert(message)
+var name ="Arbash";
+var age ="24 years old";
+var course ="Saylani Web app development";
+alert(name)
+alert(age)
+alert(course)
+var email ="arbashhumayun0@gmail.com"
+alert("My Email Address is" +'\xa0'+ email)
+var book ="A Smart Way to Learn JavaScript"
+alert(book)
+document.write("yah! I can write HTML content with JavaScript")
+
